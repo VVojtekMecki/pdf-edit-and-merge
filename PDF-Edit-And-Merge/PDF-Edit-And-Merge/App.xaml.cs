27d2aedf60d962ee3a4a654eb58519cb429e8ac2
Wojciech Medenecki <wojmed@st.amu.edu.pl>
2024-08-09 21:17:08 +0200

    init

---
+++ PDF-Edit-And-Merge/PDF-Edit-And-Merge/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PDF_Edit_And_Merge
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
