07a90405d30975f32ccd185831e6e6f49a21e0a4
Wojciech Medenecki <wojmed@st.amu.edu.pl>
2024-08-10 17:11:28 +0200

    early grid, can add files to the collection

--- PDF-Edit-And-Merge/MainPage.xaml.cs
+++ PDF-Edit-And-Merge/MainPage.xaml.cs
@@ -4,6 +4,9 @@ namespace PDF_Edit_And_Merge
 {
     public partial class MainPage : ContentPage
     {
+        private double headingHeight = 50;
+        private double upperActionsRowHeight = 50;
+        private double paddingValue = 20;
         private readonly MainPageViewModel viewModel;
 
         public MainPage(MainPageViewModel vm)
@@ -11,6 +14,18 @@ namespace PDF_Edit_And_Merge
             InitializeComponent();
             viewModel = vm;
             BindingContext = vm;
+        }
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            var contentHeight = height - headingHeight - upperActionsRowHeight - 2 * paddingValue;
+            ContentGrid.Padding = paddingValue;
+            ContentGrid.RowDefinitions = [
+                new RowDefinition(headingHeight),
+                new RowDefinition(upperActionsRowHeight),
+                new RowDefinition(contentHeight*0.8),
+                new RowDefinition(contentHeight*0.2)
+                ];
         }
 
     }
